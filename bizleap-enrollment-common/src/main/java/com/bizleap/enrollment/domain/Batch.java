0164176d17fd2faf6166f0be18e0d34012393d7e
Thet Min Wai <thetminwai17@gmail.com>
2019-12-03 13:48:04 +0630

    update batch,payment

--- bizleap-enrollment-common/src/main/java/com/bizleap/enrollment/domain/Batch.java
+++ bizleap-enrollment-common/src/main/java/com/bizleap/enrollment/domain/Batch.java
@@ -9,15 +9,20 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Entity
 @Table(name = "batch")
 public class Batch extends AbstractEntity {
+	
+	@Temporal(TemporalType.TIME)
 	@Column(name = "startDate")
 	Date startDate;
 	
+	@Temporal(TemporalType.TIME)
 	@Column(name = "endDate")
 	Date endDate;
 
